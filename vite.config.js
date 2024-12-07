9fe264715a128d9482f87f96882dcd24be397861
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-12-08 01:21:33 +0300

    debug routes not working

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import path from "path"
+import { fileURLToPath } from 'url'
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
