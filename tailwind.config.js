d9bc448d0e54c4a25ad1168755aed654e48d7e12
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2025-05-05 17:53:00 +0300

    fix: update background color in Tailwind configuration for consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
   			sm: 'calc(var(--radius) - 4px)'
   		},
   		colors: {
-  			background: '#E6F0FF',
+  			background: '#F8F9FA',
   			foreground: 'hsl(var(--foreground))',
   			card: {
   				DEFAULT: 'hsl(var(--card))',
